a321ebbc1275957f5f2d537dbad1c947bb261bf3
Collins Kariuki <collinsmunkary@gmail.com>
2022-09-11 10:21:36 +0300

    exporting and requiring a dirctory making use of the index.js file recognised by Node

--- Modules_NPM/app.js
+++ Modules_NPM/app.js
@@ -1,5 +1,8 @@
 const math = require("./math");
+const cats = require("../shelter")
 
-console.log(math.PI);
+// console.log(math.PI);
 
-console.log(math.square(9));
+// console.log(math.square(9));
+
+console.log("REQUIRED AN ENTIRE DIRECTORY", cats);
--- Modules_NPM/math.js
+++ Modules_NPM/math.js
@@ -4,13 +4,18 @@ const PI = 3.14159;
 
 const square = x => x * x;
 
-const math = {
-    add: add,
-    PI: PI,
-    square: square
-}
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+// module.exports = math;
 
-module.exports = math;
+
+//a more succint way of exporting
+exports.add = add;
+exports.square = square;
+exports.PI = PI;
 
 //exporting modules
 // module.exports.add = add;
